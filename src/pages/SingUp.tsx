2276f01f11b4815f3b4a8b7123f30f6b104c5cf8
santiagoRicops <santuago231@gmail.com>
2023-12-08 22:01:28 -0500

    Attempts are being made to verify accounts that have already been registered

--- src/pages/SingUp.tsx
+++ src/pages/SingUp.tsx
@@ -1,49 +1,115 @@
 import '../styles/sign-up.css'
 import LogoGoogle from '../assets/Google-logo.png'
-import LogoApple from '../assets/Apple-logo.png'
+import { FormEvent } from 'react'
+import { signInWithPopup } from 'firebase/auth'
+import { auth, provider } from '../firebase/firabase'
+import {
+   createUserWithEmailAndPassword,
+   signInWithEmailAndPassword,
+} from 'firebase/auth'
+import { useState } from 'react'
+
 const SignUp = () => {
-  return (
-    <section>
-      <div>
-        <header>
-          <h1>Social-Network</h1>
-        </header>
-        <div>
-          <div className='conteinerFormSingUp'>
-            <form className='formSingUp'>
-              <div className='user-box'>
-                <input type='text' name='' required />
-                <label>Username</label>
-              </div>
-              <div className='user-box'>
-                <input type='email' name='' required />
-                <label>Email</label>
-              </div>
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const [veri, setVeri] = useState(false)
 
-              <div className='user-box'>
-                <input type='password' name='' required />
-                <label>Password</label>
-              </div>
+   const handleSignPopup = async () => {
+      const signGoogle = await signInWithPopup(auth, provider)
+      const result = signGoogle.user
+      if (signGoogle) {
+         console.log(result.displayName)
+      } else {
+         console.log(result.displayName)
+      }
+   }
+   const registerUserEmailAndPassword = async (event: FormEvent) => {
+      event.preventDefault()
+      const registerUserEmail = await createUserWithEmailAndPassword(
+         auth,
+         email,
+         password,
+      )
+      if (registerUserEmail) {
+         console.log(registerUserEmail)
+      }
+   }
 
-              <button type='submit'>Sing Up</button>
-            </form>
-            <div className='OtherLoginOptions'>
-              <div className='line'></div>
-              <p>Or Sign Up with </p>
-              <div className='line'></div>
+   //Check if the user is already registered
+   const handleCheckIfTheUserIsAlreadyRegistered = async () => {
+      try {
+         await signInWithEmailAndPassword(auth, email, password)
+         setVeri(true)
+      } catch (error) {
+         console.log('hay un error' + error)
+      }
+   }
+
+   // const  handleValueEmail = (event: Event) =>{
+   //  const a = event.target as HTMLInputElement
+   // setPassword(a.value)
+   // }
+   return (
+      <section>
+         <div>
+            <header>
+               <h1>Social-Network</h1>
+            </header>
+            <h3>{veri ? 'usuario ya registrado' : ''}</h3>
+            <div>
+               <div className='conteinerFormSingUp'>
+                  <form
+                     className='formSingUp'
+                     onSubmit={registerUserEmailAndPassword}
+                  >
+                     <div className='user-box'>
+                        <input type='text' name='' required />
+                        <label>Username</label>
+                     </div>
+
+                     <div className='user-box'>
+                        <input
+                           type='email'
+                           name=''
+                           required
+                           onChange={(value) => setEmail(value.target.value)}
+                           value={email}
+                        />
+                        <label>Email</label>
+                     </div>
+                     <div className='user-box'>
+                        <input
+                           type='password'
+                           name=''
+                           required
+                           onChange={(value) => setPassword(value.target.value)}
+                           value={password}
+                        />
+                        <label>Password</label>
+                     </div>
+
+                     <button
+                        type='submit'
+                        onClick={handleCheckIfTheUserIsAlreadyRegistered}
+                     >
+                        Sing Up
+                     </button>
+                  </form>
+                  <div className='OtherLoginOptions'>
+                     <div className='line'></div>
+                     <p>Or Sign Up with </p>
+                     <div className='line'></div>
+                  </div>
+                  <div className='imgLogos'>
+                     <figure className='logo' onClick={handleSignPopup}>
+                        <img src={LogoGoogle} alt='logo google' />
+                     </figure>
+                  </div>
+               </div>
             </div>
-            <div className='imgLogos'>
-              <figure className='logo'>
-                <img src={LogoGoogle} alt='' />
-              </figure>
-              <figure className='logo'>
-                <img src={LogoApple} alt='' />
-              </figure>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+         </div>
+      </section>
+   )
 }
+
 export default SignUp
